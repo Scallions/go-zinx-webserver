2de99cbb0d273d5d5c46ff1e2e68c276d3177cf4
Scallions <844120806@qq.com>
2021-06-23 10:05:38 +0800

    v0.3

---
+++ zinx/znet/server.go
@@ -0,0 +1,98 @@
+package znet
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"time"
+
+	"../ziface"
+)
+
+type Server struct {
+	Name string
+	IPVersion string
+	IP string
+	Port int
+	Router ziface.IRouter
+}
+
+func NewServer(name string) ziface.IServer {
+	s := &Server{
+		Name: name,
+		IPVersion: "tcp4",
+		IP: "0.0.0.0",
+		Port: 7777,
+		Router:  nil,
+	}
+	return s
+}
+
+func (s *Server) Start() {
+	fmt.Printf("[START] Server listenner at IP: %s, Port %d, is starting\n", s.IP, s.Port)
+
+	go func() {
+		addr, err := net.ResolveTCPAddr(s.IPVersion, fmt.Sprintf("%s:%d", s.IP, s.Port))
+		if err != nil {
+			fmt.Println("resolve tcp addr err: ", err)
+		}
+
+		listenner, err := net.ListenTCP(s.IPVersion, addr)
+		if err != nil {
+			fmt.Println("listen", s.IPVersion, "err", err)
+			return 
+		}
+
+		fmt.Println("start Zinx server ", s.Name, " succ, now listenning...")
+
+		// TODO: 自动生成id方法
+		var cid uint32 = 0
+
+		for {
+			conn, err := listenner.AcceptTCP()
+			if err != nil {
+				fmt.Println("Accept err ", err)
+				continue
+			}
+
+			// TODO: 最大连接限制
+
+			dealConn := NewConnection(conn, cid, s.Router)
+			cid++
+			go dealConn.Start()
+		}
+	}()
+}
+
+func (s *Server) Stop() {
+	fmt.Println("[STOP] Zinx server, name ", s.Name)
+	// TODO: 清理其他信息，连接信息等
+}
+
+func (s *Server) Serve() {
+	s.Start()
+
+	// TODO: 处理其他事务
+
+	for {
+		time.Sleep(10*time.Second)
+	}
+}
+
+func (s *Server) AddRouter(router ziface.IRouter) {
+	s.Router = router
+	fmt.Println("Add Router succ! ")
+}
+
+
+// ========== 定义 handle api ===============
+// 回显
+func CallBackToClient(conn *net.TCPConn, data []byte, cnt int) error {
+	fmt.Println("[Conn Handle] CallBackToClient ... ")
+	if _, err := conn.Write(data[:cnt]); err != nil {
+		fmt.Println("write back buf err ", err)
+		return errors.New("CallBackToClient error")
+	}
+	return nil 
+}
+
